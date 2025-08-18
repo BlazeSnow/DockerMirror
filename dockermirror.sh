4285c5b01b3413051740f62e7f791f3ea3e437c1
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-08-18 11:12:57 +0800

    Dev (#2)
    * update
    
    * Update dockermirror.sh
    
    * Update dockermirror.sh
    
    * update
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update dockermirror.sh

---
+++ dockermirror.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+SOURCE="${SOURCE}"
+TARGET="${TARGET}"
+
+echo "Pulling image $SOURCE"
+
+docker pull $SOURCE
+
+echo "Tagging $SOURCE to $TARGET"
+
+docker tag $SOURCE $TARGET
+
+echo "Pushing $TARGET"
+
+docker push $TARGET
+
+echo "Cleaning"
+
+docker system prune -a -f
+
+echo "Done"
