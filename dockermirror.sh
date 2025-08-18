0a4cdc5f5e9546f0e63a9759bd5a549d0e25c0ec
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-08-18 12:16:18 +0800

    Dev (#4)
    * Update dockermirror.sh
    
    * Update dockermirror.sh
    
    * Update CHANGELOG.md

--- dockermirror.sh
+++ dockermirror.sh
@@ -12,20 +12,28 @@ for i in $(seq 0 $((count - 1))); do
     SOURCE="docker.io/$(jq -r ".[$i].source" images.json)"
     TARGET="$REGISTRY/$NAMESPACE/$(jq -r ".[$i].target" images.json)"
 
+    # 分隔符
+    echo ""
+    echo "----------------------------------------"
+    echo "源镜像：$SOURCE"
+    echo "目的地：$TARGET"
+    echo "----------------------------------------"
+    echo ""
+
     # 拉取镜像
-    echo "⬇️ 拉取 $SOURCE"
+    echo "⬇️ 拉取镜像"
     docker pull --quiet "$SOURCE"
 
     # 重命名镜像
-    echo "🔄 重命名 $SOURCE 为 $TARGET"
-    docker tag "$SOURCE" "$TARGET"
+    echo "🔄 重命名镜像"
+    docker tag "$SOURCE" "$TARGET" >/dev/null
 
     # 推送镜像
-    echo "⬆️ 推送 $TARGET"
+    echo "⬆️ 推送镜像"
     docker push --quiet "$TARGET"
 
     # 清理镜像
     echo "🧹 清理镜像"
-    docker system prune -a -f
+    docker system prune -a -f >/dev/null
 
 done
