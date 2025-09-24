0d8e5a68e1c16a41a373fb116795007c70a2898d
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-09-24 12:08:40 +0800

    Dev (#5)
    * Rename and update Docker workflow to mirror.yml
    
    Renamed the GitHub Actions workflow file from docker.yml to mirror.yml. Updated environment variable usage by hardcoding registry, namespace, and username values, and changed the secret for the password to AlibabaTOKEN.
    
    * Refactor registry variables in mirror workflow and script
    
    Replaces separate REGISTRY and NAMESPACE environment variables with a single HEAD variable in the GitHub Actions workflow. Updates dockermirror.sh to use HEAD for target image paths and removes 'docker.io/' prefix from source images, simplifying configuration and usage.

--- dockermirror.sh
+++ dockermirror.sh
@@ -8,32 +8,32 @@ count=$(jq '. | length' images.json)
 # 循环处理
 for i in $(seq 0 $((count - 1))); do
 
-    # 设定变量
-    SOURCE="docker.io/$(jq -r ".[$i].source" images.json)"
-    TARGET="$REGISTRY/$NAMESPACE/$(jq -r ".[$i].target" images.json)"
+	# 设定变量
+	SOURCE="$(jq -r ".[$i].source" images.json)"
+	TARGET="$HEAD/$(jq -r ".[$i].target" images.json)"
 
-    # 分隔符
-    echo ""
-    echo "----------------------------------------"
-    echo "源镜像：$SOURCE"
-    echo "目的地：$TARGET"
-    echo "----------------------------------------"
-    echo ""
+	# 分隔符
+	echo ""
+	echo "----------------------------------------"
+	echo "源镜像：$SOURCE"
+	echo "目的地：$TARGET"
+	echo "----------------------------------------"
+	echo ""
 
-    # 拉取镜像
-    echo "⬇️ 拉取镜像"
-    docker pull --quiet "$SOURCE"
+	# 拉取镜像
+	echo "⬇️ 拉取镜像"
+	docker pull --quiet "$SOURCE"
 
-    # 重命名镜像
-    echo "🔄 重命名镜像"
-    docker tag "$SOURCE" "$TARGET" >/dev/null
+	# 重命名镜像
+	echo "🔄 重命名镜像"
+	docker tag "$SOURCE" "$TARGET" >/dev/null
 
-    # 推送镜像
-    echo "⬆️ 推送镜像"
-    docker push --quiet "$TARGET"
+	# 推送镜像
+	echo "⬆️ 推送镜像"
+	docker push --quiet "$TARGET"
 
-    # 清理镜像
-    echo "🧹 清理镜像"
-    docker system prune -a -f >/dev/null
+	# 清理镜像
+	echo "🧹 清理镜像"
+	docker system prune -a -f >/dev/null
 
 done
